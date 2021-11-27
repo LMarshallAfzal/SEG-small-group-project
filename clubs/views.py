a571f1c08ab227e43f535ae5d6672cb75e1126f5
Encheng Wu <encheng.wu@kcl.ac.uk>
2021-11-27 02:59:59 +0000

    -"Change the log in page to the user profile page" -"Make the Chess web app button redirect to the current user profile page" -"Members button redirect to the member list" -"Not able to check other members email,personal statement and experience level"

--- clubs/views.py
+++ clubs/views.py
@@ -10,6 +10,7 @@ from django.contrib.auth.models import Group
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponseForbidden
 from django.shortcuts import redirect, render
+from .helpers import login_prohibited
 
 def log_in(request):
     if request.method == 'POST':
@@ -22,12 +23,12 @@ def log_in(request):
                 if user.groups.filter(name = 'Officer'):
                     #user.groups.filter(name ='Member').exists()
                     login(request, user)
-
                     redirect_url = request.POST.get('next') or 'officer'
                     return redirect(redirect_url)
                     """View for member"""
                 elif user.groups.filter(name = 'Member'):
-                    pass
+                    login(request, user)
+                    return redirect('show_current_user_profile')
                     """View for owner"""
                 elif user.groups.filter(name = 'Owner'):
                     pass
@@ -40,13 +41,21 @@ def log_in(request):
     next = request.GET.get('next') or 'officer'
     return render(request, 'log_in.html', {'form': form, 'next' : next})
 
+
 def log_out(request):
     logout(request)
     return redirect('home')
 
+@login_prohibited
 def home(request):
     return render(request, 'home.html')
 
+@login_required
+def show_current_user_profile(request):
+    current_user = request.user
+    return render(request, 'show_current_user_profile.html', {'user': current_user})
+
+@login_prohibited
 def sign_up(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
@@ -58,14 +67,28 @@ def sign_up(request):
         form = SignUpForm()
     return render(request, 'sign_up.html', {'form': form})
 
+@login_required
+def profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = UserForm(instance=current_user, data=request.POST)
+        if form.is_valid():
+            messages.add_message(request, messages.SUCCESS, "Profile updated!")
+            form.save()
+            return redirect('member_list')#depends on the user type
+    else:
+        form = UserForm(instance=current_user)
+    return render(request, 'profile.html', {'form': form})
+
+@login_required
 def member_list(request):
-    users = User.objects.filter(groups__name = 'Member'); # This ensures only members on shown
+    users = User.objects.all();
     return render(request, 'member_list.html', {'users': users})
 
-# def show_user(request, user_id):
-#     User = get_user_model()
-#     user = User.objects.get(id = user_id)
-#     return render(request, 'show_user.html', {'user' : user})
+def show_user(request, user_id):
+    User = get_user_model()
+    user = User.objects.get(id = user_id)
+    return render(request, 'show_user.html', {'user' : user})
 
 def officer_main(request):
     users = User.objects.filter(groups__name__in=['Owner', 'Member', 'Officer'])
