30f19f2d3939bbddb154590db8a8cf17762b7ae4
“Kyal <kyal.patel@kcl.ac.uk>
2021-11-29 14:14:45 +0000

    Partial conversion of groups.py to 'template' class

--- clubs/groups.py
+++ clubs/groups.py
@@ -1,78 +1,80 @@
-"""Creates groups for the initial club - may be able to modify to make this a "template" for all clubs"""
+"""Creates groups and assigns permissions for the groups for a chess club"""
 from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
 from clubs.models import User
 
-applicant_group, created = Group.objects.get_or_create(name = 'Applicant')
-member_group, created = Group.objects.get_or_create(name = 'Member')
-officer_group, created = Group.objects.get_or_create(name = 'Officer')
-owner_group, created = Group.objects.get_or_create(name = 'Owner')
+#The club name is passed in to be used as a way of separating groups and permissions for each club
+class ChessClubGroups(chess_club_name):
+    applicant_group, created = Group.objects.get_or_create(name = chess_club_name + ' Applicant')
+    member_group, created = Group.objects.get_or_create(name = chess_club_name + ' Member')
+    officer_group, created = Group.objects.get_or_create(name = chess_club_name + ' Officer')
+    owner_group, created = Group.objects.get_or_create(name = chess_club_name + ' Owner')
 
-user_content_type = ContentType.objects.get_for_model(User)
+    user_content_type = ContentType.objects.get_for_model(User)
 
-#member_list_permission = Permission.objects.create(
-#    codename = 'can_access_member_list',
-#    name = 'Can access a basic list of members and some details',
-#    content_type = user_content_type,
-#)
-member_list_permission = Permission.objects.get(codename = 'can_access_member_list')
-member_group.permissions.add(member_list_permission)
-officer_group.permissions.add(member_list_permission)
-owner_group.permissions.add(member_list_permission)
+    member_list_permission = Permission.objects.create(
+        codename = 'can_access_member_list_' + chess_club_name,
+        name = 'Can access a basic list of members and some details for club ' + chess_club_name,
+        content_type = user_content_type,
+    )
+    #member_list_permission = Permission.objects.get(codename = 'can_access_member_list')
+    member_group.permissions.add(member_list_permission)
+    officer_group.permissions.add(member_list_permission)
+    owner_group.permissions.add(member_list_permission)
 
-#full_member_list_permission = Permission.objects.create(
-#    codename = 'can_access_full_member_list',
-#    name = 'Can access list of members with all information',
-#    content_type = user_content_type,
-#)
-full_member_list_permission = Permission.objects.get(codename = 'can_access_full_member_list')
-officer_group.permissions.add(full_member_list_permission)
-owner_group.permissions.add(full_member_list_permission)
+    full_member_list_permission = Permission.objects.create(
+        codename = 'can_access_full_member_list_' + chess_club_name,
+        name = 'Can access list of members with all information for club ' + chess_club_name,
+        content_type = user_content_type,
+    )
+    #full_member_list_permission = Permission.objects.get(codename = 'can_access_full_member_list')
+    officer_group.permissions.add(full_member_list_permission)
+    owner_group.permissions.add(full_member_list_permission)
 
-# applications_permission = Permission.objects.create(
-#     codename = 'can_accept_applications',
-#     name = 'Can allow an applicant to become a member',
-#     content_type = user_content_type,
-# )
-applications_permission = Permission.objects.get(codename = 'can_accept_applications')
-officer_group.permissions.add(applications_permission)
+     applications_permission = Permission.objects.create(
+         codename = 'can_accept_applications_' + chess_club_name,
+         name = 'Can allow an applicant to become a member for club ' + chess_club_name,
+         content_type = user_content_type,
+     )
+    #applications_permission = Permission.objects.get(codename = 'can_accept_applications')
+    officer_group.permissions.add(applications_permission)
 
-# remove_member_permission = Permission.objects.create(
-#     codename = 'can_remove_member',
-#     name = 'Can remove a member from the club',
-#     content_type = user_content_type,
-# )
-remove_member_permission = Permission.objects.get(codename = 'can_remove_member')
-officer_group.permissions.add(remove_member_permission)
+    # remove_member_permission = Permission.objects.create(
+    #     codename = 'can_remove_member',
+    #     name = 'Can remove a member from the club',
+    #     content_type = user_content_type,
+    # )
+    remove_member_permission = Permission.objects.get(codename = 'can_remove_member')
+    officer_group.permissions.add(remove_member_permission)
 
-# promote_permission = Permission.objects.create(
-#     codename = 'can_promote_member',
-#     name = 'Can promote a member to an officer',
-#     content_type = user_content_type,
-# )
-promote_permission = Permission.objects.get(codename = 'can_promote_member')
-officer_group.permissions.add(promote_permission)
+    # promote_permission = Permission.objects.create(
+    #     codename = 'can_promote_member',
+    #     name = 'Can promote a member to an officer',
+    #     content_type = user_content_type,
+    # )
+    promote_permission = Permission.objects.get(codename = 'can_promote_member')
+    officer_group.permissions.add(promote_permission)
 
-# demote_permission = Permission.objects.create(
-#     codename = 'can_demote_officer',
-#     name = 'Can demote an officer to a member',
-#     content_type = user_content_type,
-# )
-demote_permission = Permission.objects.get(codename = 'can_demote_officer')
-owner_group.permissions.add(demote_permission)
+    # demote_permission = Permission.objects.create(
+    #     codename = 'can_demote_officer',
+    #     name = 'Can demote an officer to a member',
+    #     content_type = user_content_type,
+    # )
+    demote_permission = Permission.objects.get(codename = 'can_demote_officer')
+    owner_group.permissions.add(demote_permission)
 
-# ownership_permission = Permission.objects.create(
-#     codename = 'can_transfer_ownership',
-#     name = 'Can transfer owner status to an officer',
-#     content_type = user_content_type,
-# )
-ownership_permission = Permission.objects.get(codename = 'can_transfer_ownership')
-owner_group.permissions.add(ownership_permission)
+    # ownership_permission = Permission.objects.create(
+    #     codename = 'can_transfer_ownership',
+    #     name = 'Can transfer owner status to an officer',
+    #     content_type = user_content_type,
+    # )
+    ownership_permission = Permission.objects.get(codename = 'can_transfer_ownership')
+    owner_group.permissions.add(ownership_permission)
 
-# become_owner_permission = Permission.objects.create(
-#     codename = 'can_become_owner',
-#     name = 'Can receive ownership of club',
-#     content_type = user_content_type,
-# )
-become_owner_permission = Permission.objects.get(codename = 'can_become_owner')
-officer_group.permissions.add(become_owner_permission)
+    # become_owner_permission = Permission.objects.create(
+    #     codename = 'can_become_owner',
+    #     name = 'Can receive ownership of club',
+    #     content_type = user_content_type,
+    # )
+    become_owner_permission = Permission.objects.get(codename = 'can_become_owner')
+    officer_group.permissions.add(become_owner_permission)
