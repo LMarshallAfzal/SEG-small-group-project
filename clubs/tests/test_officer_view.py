387ae65909f0defb5c84a3ac58d41f78c23643f3
Guneek Deol <guneek.deol@kcl.ac.uk>
2021-11-30 17:46:39 +0000

    fix group error, add redirect and template testing

--- clubs/tests/test_officer_view.py
+++ clubs/tests/test_officer_view.py
@@ -6,6 +6,7 @@ from clubs.forms import LogInForm
 from clubs.models import User
 from .helpers import LogInTester
 from django.contrib.auth.models import Group
+from clubs.groups import Group
 
 class OfficerViewTestCase(TestCase):
     """Tests of the officer view"""
@@ -26,19 +27,25 @@ class OfficerViewTestCase(TestCase):
         self.assertEqual(self.url, '/officer/')
 
     def test_applicant_can_be_accepted(self):
-
+        response = self.client.post(self.url, self.user.id, follow = True)
         self.member.user_set.add(self.user)
+        response_url = reverse('officer_promote_applicant')
+        self.assertRedirects(response, response_url, status_code = 302, target_status_code = 200)
+        self.assertTemplateUsed(response, 'officer_promote_applicant.html')
         self.applicant.user_set.remove(self.user)
-        self.assertNotIn(self.user,self.applicant)
-        self.asserIn(self.user,self.member)
-
+        self.assertFalse(self.user.groups.filter(name = 'Applicant').exists())
+        self.assertTrue(self.user.groups.filter(name='Member').exists())
+        
     def test_applicant_can_be_rejected(self):
         before_count = User.objects.count()
         self.applicant.user_set.remove(self.user)
         User.objects.remove(self.user)
-        self.assertNotIn(self.user,self.applicant)
+        self.assertFalse(self.user.groups.filter(name='Applicant').exists())
         after_count = User.objects.count()
         self.assertEqual(after_count,before_count-1)
+        response = self.client.post(self.url, self.user.id)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'officer_promote_applicant.html')
 
 
       
