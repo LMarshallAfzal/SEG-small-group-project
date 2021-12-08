8f4cf2cb5197d95d747c7259f12ee110341d6bd9
Zaid Maraqa <zaid.maraqa@kcl.ac.uk>
2021-12-08 18:29:31 +0000

    Refactor sign up and owner view

--- clubs/tests/test_owner_view.py
+++ clubs/tests/test_owner_view.py
@@ -8,36 +8,37 @@ from clubs.models import User
 from clubs.views import officer
 from .helpers import LogInTester
 from django.contrib.auth.models import Group
+from clubs.club_list import ClubList
 
 class UserFormTestCase(TestCase):
     """Unit tests of the user form."""
 
     fixtures = [
-        'clubs/tests/fixtures/default_user.json',    
+        'clubs/tests/fixtures/default_user.json',
         'clubs/tests/fixtures/other_user.json'
     ]
-    
+
 
     def setUp(self):
+        list_of_clubs = ClubList()
+        list_of_clubs.create_new_club("waterBoys")
+        club = list_of_clubs.find_club("waterBoys")
         self.user = User.objects.get(username='johndoe@example.org')
         self.url = reverse('owner')
         self.other_user = User.objects.get(username = 'janedoe@example.org')
 
-        member = Group.objects.get(name = "Member")
+        member = club.getClubMemberGroup()
         member.user_set.add(self.user)
         member.user_set.add(self.other_user)
 
-        officer = Group.objects.get(name = "Officer")
+        officer = club.getClubOfficerGroup()
         member.user_set.add(self.user)
         officer.user_set.add(self.other_user)
 
-        owner = Group.objects.get(name = "Owner")
+        owner = club.getClubOwnerGroup()
         member.user_set.add(self.user)
         owner.user_set.add(self.user)
 
-
-        
-        
 
     def test_owner_url(self):
         self.assertEqual(self.url,'/owner/')
@@ -47,24 +48,24 @@ class UserFormTestCase(TestCase):
         self.assertEqual(response.status_code,200)
         self.assertTemplateUsed('owner.html')
 
-    
+
     """inherit from officer test"""
     def test_promote_member(self):
             pass
-        
 
-    
+
+
     def test_can_only_promote_officer_to_owner(self):
         self.officer.user_set.remove(self.other_user)
         self.assertFalse(self.other_user.groups.filter(name='Officer').exists())
         self.owner.user_set.add(self.other_user)
-        self.assertFalse(self.other_user.groups.filter(name='Owner').exists())  
+        self.assertFalse(self.other_user.groups.filter(name='Owner').exists())
         response = self.client.post(self.url,self.other_user.id)
         response_url = reverse('member_list')
         self.assertRedirects(response,response_url,status_code= 302, target_status_code= 200)
         self.assertTemplateUsed(response,'member_list.html')
 
-    
+
     def test_officer_can_be_promoted(self):
         self.assertTrue(self.other_user_officer)
         self.owner.user_set.add(self.other_user)
@@ -85,9 +86,3 @@ class UserFormTestCase(TestCase):
         owners = self.owner.user_set.getAll.toList
         current_owner = owners[0]
         self.assertEqual(owners[0],self.other_user)
-    
-    
-        
-        
-        
-        
