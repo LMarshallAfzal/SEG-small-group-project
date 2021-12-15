bdc6b405745b4e4e598e189e5518001f3437ff54
“Kyal <kyal.patel@kcl.ac.uk>
2021-12-15 09:09:30 +0000

    Add tests for application form

--- clubs/tests/forms/test_club_application_form.py
+++ clubs/tests/forms/test_club_application_form.py
@@ -1,12 +1,20 @@
 """Tests for the club application form"""
 from clubs.forms import ApplicationForm
-from clubs.models import User
+from clubs.models import Club
 from django.test import TestCase
 from django import forms
+from clubs.club_list import ClubList
 
 class ClubApplicationFormTestCase(TestCase):
     """Tests for the club application form"""
     def setUp(self):
+        self.list_of_clubs = ClubList()
+        self.list_of_clubs.create_new_club(
+            name = "The Buzzards",
+            mission_statement = "Scavenging wastelander tribe of bandits",
+            location = "The sunken city"
+        )
+        self.club = list_of_clubs.find_club("The Buzzards")
         self.form_input = {
             "first_name": 'Jack',
             "last_name": 'Henwood',
@@ -28,3 +36,26 @@ class ClubApplicationFormTestCase(TestCase):
         email_field = form.fields['email']
         self.assertTrue(isinstance(email_field, forms.EmailField))
         self.assertIn('bio', form.fields)
+        self.assertIn('experience_level', form.fields)
+        self.assertIn('personal_statement', form.fields)
+
+    def test_form_uses_model_validation(self):
+        self.form_input['email'] = 'BAD_EMAIL@@email.org'
+        form = ApplicationForm(data = self.form_input)
+        self.assertFalse(form.is_valid())
+
+    # def test_form_saves_correctly(self):
+    #     form = ApplicationForm(data = self.form_input)
+    #     before_role = Club.
+    #     form.save()
+    #     after_count = User.objects.count()
+    #     self.assertEqual(after_count, before_count + 1)
+    #     user = User.objects.get(first_name = 'Jack')
+    #     self.assertEqual(user.first_name, 'Jack')
+    #     self.assertEqual(user.last_name, 'Henwood')
+    #     self.assertEqual(user.username, 'jackhenwood@example.org')
+    #     self.assertEqual(user.bio, 'My bio')
+    #     self.assertEqual(user.experience_level, 'Beginner')
+    #     self.assertEqual(user.personal_statement, 'My personal statement!')
+    #     is_password_correct = check_password('Password123', user.password)
+    #     self.assertTrue(is_password_correct)
