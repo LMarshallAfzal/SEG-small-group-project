e234a23e3bee9637659719a346ae57fb91302058
“Kyal <kyal.patel@kcl.ac.uk>
2021-12-15 12:32:01 +0000

    Fix pull glitch causing conflict markers in file

--- clubs/tests/views/test_club_application_view.py
+++ clubs/tests/views/test_club_application_view.py
@@ -10,16 +10,11 @@ def ClubApplicationViewTestCase(TestCase):
     fixtures = [
         'clubs/tests/fixtures/default_user.json',
         'clubs/tests/fixtures/other_user.json',
-<<<<<<< HEAD
         'clubs/tests/fixtures/club.json'
-=======
-        'clubs/tests/fixtures/club.json',
->>>>>>> bdc6b405745b4e4e598e189e5518001f3437ff54
     ]
 
     def setUp(self):
         self.url = reverse('application_form')
-<<<<<<< HEAD
         #Form input based on user fixture
         self.form_input = {
             "first_name": 'John',
@@ -28,22 +23,8 @@ def ClubApplicationViewTestCase(TestCase):
             "bio": 'John Doe from example.org',
             "experience_level": 'Beginner',
             "personal_statement": 'I love chess'
-=======
-        self.form_input = {
-            "first_name": 'Jack',
-            "last_name": 'Henwood',
-            "email": 'jackhenwood@example.org',
-            "bio": 'My bio',
-            "experience_level": 'Beginner',
-            "personal_statement": 'My personal statement!'
->>>>>>> bdc6b405745b4e4e598e189e5518001f3437ff54
         }
 
 
     """The fields should be filled in automatically when a user enters this form,
-<<<<<<< HEAD
        as they would be logged in already, how to do this?"""
-=======
-       as they would be logged in already. How to test this likely involves creating
-       and logging in a user first, how to do this?"""
->>>>>>> bdc6b405745b4e4e598e189e5518001f3437ff54
