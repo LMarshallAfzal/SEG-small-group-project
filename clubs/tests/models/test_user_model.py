ffef5a570c684a3fb263a521e88e242e10d5d98e
Guneek <60737783+Guneek@users.noreply.github.com>
2021-12-17 15:32:20 +0000

    Update test_user_model.py

--- clubs/tests/models/test_user_model.py
+++ clubs/tests/models/test_user_model.py
@@ -88,6 +88,8 @@ class UserModelTestCase(TestCase):
     def test_email_is_not_case_sensitive(self):
         second_user = self._create_second_user()
         second_user.email = 'JARREDBOWEN@example.org'
+        self.client.login(email = second_user.email, password = "Password123")
+        self.asertTrue(self._is_logged_in())
         
 
     """Tests for the bio field"""
